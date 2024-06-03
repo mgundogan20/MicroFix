cc0f61e5d4bbd3e1d8d235e684551cc444d4541d
mgundogan20 <mgundogan20@ku.edu.tr>
2024-06-03 12:15:50 +0300

    Better loss function and batches

--- finetune_net_SR.py
+++ finetune_net_SR.py
@@ -9,6 +9,7 @@ import glob
 import numpy as np
 import utils.utils_image as util
 import utils.utils_psf as util_psf
+import utils.utils_deblur as util_deblur
 import models.gan as gan
 from utils.image_pool import ImagePool
 from models.uabcnet import UABCNet as net
@@ -21,8 +22,10 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 	# Global config
 	sf = 1	
 	stage = 8
-	patch_size = [64,64]
+	n_batch = 5
+	patch_size = [128,128]
 	patch_num = [2,2]
+	disp_width = 512
 	print("Global configs set.")
 
 	# Load kernel
@@ -46,15 +49,6 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 	model = model.to(device)
 	print("Model is loaded to:", device)
 
-	#3. set up discriminator
-	model_D = gan.PatchDiscriminator(3)
-	model_D =model_D.to(device)
-
-	gan_loss = gan.GANLoss(mode='lsgan')
-	gan_loss = gan_loss.to(device)
-	fake_images = ImagePool(16)
-	print("GAN set.")
-
 
 	#positional lambda, mu for HQS.
 	if ab_path is not None:
@@ -72,11 +66,7 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 	for key,value in model.named_parameters():
 		params += [{"params":[value],"lr":1e-5}]
 
-	params_D = []
-	params_D += list(model_D.parameters())
-
 	optimizer = torch.optim.Adam(params,lr=1e-4,betas=(0.9,0.999))
-	optimizer_D = torch.optim.Adam(params_D,lr=1e-4,betas=(0.9,0.999))
 
 	scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=100,gamma=0.9)
 	print("Optimizer set")
@@ -95,14 +85,14 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 	if trainindDataLow is not None:
 		imgs_L = trainindDataLow
 		imgs_L.sort()
-		print("Low resolution training data loaded")
+		print(f"{len(imgs_L)} low resolution training images loaded.")
 
 		imgs_L_val = imgs_L[:len(imgs_L)//10]
 		imgs_L = imgs_L[len(imgs_L)//10:]
-		
+	print("\n")
 
+	avg_loss = 0
 	losses = []
-	losses_D = []
 	val_ssims = []
 	val_ssims_L = []
 	val_psnrs = []
@@ -124,8 +114,9 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 			patch_L,patch_H,patch_psf,patch_ab = util_train.draw_training_pair(img_H,PSF_grid,ab,sf,patch_num,patch_size)
 		else:
 			img_L = cv2.imread(imgs_L[img_idx])
+			patch_size = img_L.shape[0]//PSF_grid.shape[0], img_L.shape[1]//PSF_grid.shape[1]
 			patch_L,patch_H,patch_psf,patch_ab = util_train.draw_training_pair(img_H,PSF_grid,ab,sf,patch_num,patch_size, image_L=img_L)
-
+			
 		x = util.uint2single(patch_L)
 		x = util.single2tensor4(x)
 		x_gt = util.uint2single(patch_H)
@@ -148,27 +139,21 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 		x_E = model.forward_patchwise_SR(x,k,ab_patch_v,patch_num,[patch_size[0],patch_size[1]],sf)
 
 
+		x_E_grad = torch.gradient(x_E, dim=[-1, -2])
+		x_gt_grad = torch.gradient(x_gt, dim=[-1, -2])
+		loss = 2*F.mse_loss(x_E_grad[0]+x_E_grad[1],x_gt_grad[0]+x_gt_grad[1])
+
 		loss_l1 = F.l1_loss(x_E,x_gt)
-		loss_gan = gan_loss(model_D(x_E),True)
-		loss = loss_l1 + loss_gan
+		loss += (0.2*loss_l1)
 		losses.append(loss.item())
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+		avg_loss += loss / n_batch
 
+		if global_iter%n_batch==0:
+			optimizer.zero_grad()
+			avg_loss.backward()
+			optimizer.step()
+			avg_loss=0
 
-		pred_real = model_D(x_gt)
-		loss_D_real = gan_loss(pred_real,True)
-		fake = fake_images.query(x_E)
-		pred_fake = model_D(fake.detach())
-		loss_D_fake = gan_loss(pred_fake,False)
-		loss_D = (loss_D_fake+loss_D_real)*0.5
-		losses_D.append(loss_D.item())
-		optimizer_D.zero_grad()
-		loss_D.backward()
-		optimizer_D.step()
-
-		scheduler.step()
 		if global_iter%100 == 0:
 			patch_L = cv2.resize(patch_L,dsize=None,fx=sf,fy=sf,interpolation=cv2.INTER_NEAREST)
 			patch_E = util.tensor2uint((x_E))
@@ -180,26 +165,25 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 			val_psnrs.append(val_psnr)
 			val_psnrs_L.append(val_psnr_L)
 
-			if val_ssim > best_ssim:
-				best_ssim = val_ssim
-				best_model = copy.deepcopy(model.state_dict())
-				best_ab = copy.deepcopy(ab)
+			# if val_ssim > best_ssim:
+			# 	best_ssim = val_ssim
+			# 	best_model = copy.deepcopy(model)
+			# 	best_ab = copy.deepcopy(ab)
 			
 			print("Validation completed for iteration {:05d}".format(global_iter))
 
 
-	torch.save(best_model, f"{logs_directory}/models/finetuned.pth")
+	torch.save(model.state_dict(), f"{logs_directory}/models/finetuned.pth")
 	print(f"Saved the best model to {logs_directory}/models/finetuned.pth")
-	ab_numpy = best_ab.detach().cpu().numpy().flatten()
-	np.savetxt('./logs/models/ab_finetuned.txt',ab_numpy)
+	ab_numpy = ab.detach().cpu().numpy().flatten()
+	np.savetxt(f'{logs_directory}/models/ab_finetuned.txt',ab_numpy)
 	print(f"Saved the best lambda/mu parameters to {logs_directory}/models/ab_finetuned.txt")
 
 	print("Saving the loss curve to ./logs/")
 	_, (ax1, ax2, ax3) = plt.subplots(3)
 	ax1.set(ylabel="training losses")
 	ax1.plot(losses, color="red")
-	ax1.plot(losses_D, color="purple")
-	ax1.legend(["SR", "GAN"], loc="upper right", fontsize="small")
+	ax1.legend(["SR"], loc="upper right", fontsize="small")
 	
 	ax2.plot(np.linspace(0, len(losses)-1,len(val_ssims)),val_ssims)
 	ax2.plot(np.linspace(0, len(losses)-1,len(val_ssims)),val_ssims_L)
@@ -210,23 +194,25 @@ def main(trainingDataHigh, model_load_path="./logs/models/uabcnet_pre.pth", kern
 	ax3.legend(["output", "input"], loc="lower right", fontsize="small")
 	ax3.set(ylabel="validation PSNRs")
 	
-	plt.savefig("./logs/finetuning.png")
-	# plt.show()
+	plt.savefig(f"{logs_directory}/finetuning.png")
+	plt.show()
 
 if __name__ == '__main__':
 	t0 = time.time()
 	print("Fine tuning the model...")
 
-	dataset = glob.glob('./images/DIV2K_train/*.png',recursive=True)
-	dataset.extend(glob.glob('./images/cell_data/*.jpeg',recursive=True))
+	dataset_H = glob.glob('./images/zemax/high/*.png',recursive=True)
+	dataset_H.sort()
+	dataset_L = glob.glob('./images/zemax/simulated/*.png',recursive=True)
+	dataset_L.sort()
 
 	main(
-		trainingDataHigh=dataset,
-		model_load_path="./logs/models/pretrained.pth",
-		kernel_path='./data/Edmund_PSF_45.npz',
-		N_maxiter=1000,
+		trainingDataHigh=dataset_H,
+		model_load_path="./logs/models/finetuned.pth",
+		kernel_path='./data/triplet_full_32x32.npz',
+		N_maxiter=2000,
 		logs_directory="./logs",
-		# ab_path="./logs/models/ab_pretrained.txt",
+		trainindDataLow=dataset_L
 		)
 	
 	deltaT = time.time() - t0
